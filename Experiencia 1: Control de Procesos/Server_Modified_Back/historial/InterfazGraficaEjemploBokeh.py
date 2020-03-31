2699a78858115e8fe7d60ce9216f6e2211b29581
mathias lambert <mglambert@uc.cl>
2020-03-31 18:13:24 -0300

    ahora si po master

--- Experiencia 1: Control de Procesos/Server_Modified_Back/historial/InterfazGraficaEjemploBokeh.py
+++ Experiencia 1: Control de Procesos/Server_Modified_Back/historial/InterfazGraficaEjemploBokeh.py
@@ -4,40 +4,44 @@ from bokeh.layouts import layout
 from bokeh.plotting import curdoc, figure
 import threading
 from cliente_control import Cliente
+from collections import deque
 
 # DataSources
-DataSource_tanque1 = ColumnDataSource(dict(time=[], ref=[], real=[]))
-DataSource_tanque2 = ColumnDataSource(dict(time=[], ref=[], real=[]))
-DataSource_tanque3 = ColumnDataSource(dict(time=[], real=[]))
-DataSource_tanque4 = ColumnDataSource(dict(time=[], real=[]))
+DataSource_tanques = ColumnDataSource(dict(time=[], ref1=[], real1=[], ref2=[], real2=[], real3=[], real4=[]))
 
 # Figuras
 # Tanque 1
 fig_tanque1 = figure(title='Tanque 1', plot_width=600, plot_height=300, y_axis_location="left", y_range=(0, 70))
-fig_tanque1.line(x='time', y='ref', alpha=0.8, line_width=3, color='black', source=DataSource_tanque1, legend='Ref')
-fig_tanque1.line(x='time', y='real', alpha=0.8, line_width=3, color='red', source=DataSource_tanque1, legend='Real')
+l1t1 = fig_tanque1.line(x='time', y='ref1', alpha=0.8, line_width=3, color='black', source=DataSource_tanques,
+                        legend='Ref')
+l2t1 = fig_tanque1.line(x='time', y='real1', alpha=0.8, line_width=3, color='red', source=DataSource_tanques,
+                        legend='Real')
 fig_tanque1.xaxis.axis_label = 'Tiempo (S)'
 fig_tanque1.yaxis.axis_label = 'Valores'
 fig_tanque1.legend.location = "top_left"
 
 # Tanque 2
 fig_tanque2 = figure(title='Tanque 2', plot_width=600, plot_height=300, y_axis_location="left", y_range=(0, 70))
-fig_tanque2.line(x='time', y='ref', alpha=0.8, line_width=3, color='black', source=DataSource_tanque2, legend='Ref')
-fig_tanque2.line(x='time', y='real', alpha=0.8, line_width=3, color='red', source=DataSource_tanque2, legend='Real')
+l1t2 = fig_tanque2.line(x='time', y='ref2', alpha=0.8, line_width=3, color='black', source=DataSource_tanques,
+                        legend='Ref')
+l2t2 = fig_tanque2.line(x='time', y='real2', alpha=0.8, line_width=3, color='red', source=DataSource_tanques,
+                        legend='Real')
 fig_tanque2.xaxis.axis_label = 'Tiempo (S)'
 fig_tanque2.yaxis.axis_label = 'Valores'
 fig_tanque2.legend.location = "top_left"
 
 # Tanque 3
 fig_tanque3 = figure(title='Tanque 2', plot_width=600, plot_height=300, y_axis_location="left", y_range=(0, 70))
-fig_tanque3.line(x='time', y='real', alpha=0.8, line_width=3, color='red', source=DataSource_tanque3, legend='Real')
+l1t3 = fig_tanque3.line(x='time', y='real3', alpha=0.8, line_width=3, color='red', source=DataSource_tanques,
+                        legend='Real')
 fig_tanque3.xaxis.axis_label = 'Tiempo (S)'
 fig_tanque3.yaxis.axis_label = 'Valores'
 fig_tanque3.legend.location = "top_left"
 
 # Tanque 4
 fig_tanque4 = figure(title='Tanque 2', plot_width=600, plot_height=300, y_axis_location="left", y_range=(0, 70))
-fig_tanque4.line(x='time', y='real', alpha=0.8, line_width=3, color='red', source=DataSource_tanque4, legend='Real')
+l1t4 = fig_tanque4.line(x='time', y='real4', alpha=0.8, line_width=3, color='red', source=DataSource_tanques,
+                        legend='Real')
 fig_tanque4.xaxis.axis_label = 'Tiempo (S)'
 fig_tanque4.yaxis.axis_label = 'Valores'
 fig_tanque4.legend.location = "top_left"
@@ -61,7 +65,7 @@ real_tanque4 = PreText(text='Valor real: 0.00', width=600, style=estilo2)
 def funcion_handler(node, val):
     key = node.get_parent().get_display_name().Text
     if key == 'Tanque1':
-        lolito = val
+        pass
 
 
 class SubHandler(object):
@@ -89,26 +93,29 @@ t = 0
 
 def MainLoop():  # Funcion principal que se llama cada cierto tiempo para mostrar la informacion
     global t
-
     h1 = cliente.alturas['H1'].get_value()
-    update = dict(time=[t], ref=[32], real=[h1])
-    DataSource_tanque1.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
-    real_tanque1.text = 'Valor real: {}'.format(round(h1, 2))
-
     h2 = cliente.alturas['H2'].get_value()
-    update = dict(time=[t], ref=[32], real=[h2])
-    DataSource_tanque2.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
-    real_tanque2.text = 'Valor real: {}'.format(round(h2, 2))
-
     h3 = cliente.alturas['H3'].get_value()
-    update = dict(time=[t], real=[h3])
-    DataSource_tanque3.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
-    real_tanque3.text = 'Valor real: {}'.format(round(h3, 2))
-
     h4 = cliente.alturas['H4'].get_value()
-    update = dict(time=[t], real=[h4])
-    DataSource_tanque4.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
-    real_tanque4.text = 'Valor real: {}'.format(round(h4, 2))
+
+    update = dict(time=[t], ref1=[50 * np.random.random()], real1=[h1],
+                  ref2=[50 * np.random.random()], real2=[h2], real3=[h3], real4=[h4])
+
+    DataSource_tanques.stream(new_data=update, rollover=20)  # Se ven los ultimos 50 datos
+    # real_tanque1.text = 'Valor real: {}'.format(round(h1, 2))
+    # real_tanque2.text = 'Valor real: {}'.format(round(h2, 2))
+    # real_tanque3.text = 'Valor real: {}'.format(round(h3, 2))
+    # real_tanque4.text = 'Valor real: {}'.format(round(h4, 2))
+    #
+    # h3 = cliente.alturas['H3'].get_value()
+    # update = dict(time=[t], real=[h3])
+    # DataSource_tanque3.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
+    # real_tanque3.text = 'Valor real: {}'.format(round(h3, 2))
+    #
+    # h4 = cliente.alturas['H4'].get_value()
+    # update = dict(time=[t], real=[h4])
+    # DataSource_tanque4.stream(new_data=update, rollover=50)  # Se ven los ultimos 100 datos
+    # real_tanque4.text = 'Valor real: {}'.format(round(h4, 2))
 
     t += 1
 
