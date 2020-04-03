579e28e5452a85781cbc3a6dca69def641281bab
mathias lambert <mglambert@uc.cl>
2020-04-03 18:20:34 -0300

    save listo

--- Experiencia 1: Control de Procesos/mathi/main.py
+++ Experiencia 1: Control de Procesos/mathi/main.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pan
 from bokeh.models import (Div, Tabs, Panel, Slider, Column, TextInput, PreText,
-                          ColumnDataSource)
+                          ColumnDataSource, Button, Select, Dropdown)
+from bokeh.events import MenuItemClick
 from bokeh.plotting import curdoc, figure
 from bokeh.layouts import layout, row
 
@@ -8,6 +10,7 @@ from cliente_control import Cliente
 import threading
 
 ''' ******************** Client ******************** '''
+
 
 def funcion_handler(node, val):
     key = node.get_parent().get_display_name().Text
@@ -35,7 +38,12 @@ class SubHandler(object):
 cliente = Cliente("opc.tcp://127.0.0.1:4840/freeopcua/server/", suscribir_eventos=True, SubHandler=SubHandler)
 cliente.conectar()
 
+# cliente.subscribir_mv() # Se subscribe a las variables manipuladas
+# cliente.subscribir_cv() # Se subscribe a las variables controladas
+
+
 '''************************ Valores iniciales y variables globales ***************'''
+
 ref1 = 40
 ref2 = 40
 vol1 = cliente.valvulas['valvula1'].get_value()
@@ -45,36 +53,51 @@ gamma2 = cliente.razones['razon2'].get_value()
 
 t = 0
 automatico = True
+datos_a_guardar = None
+extension = None
+n_archivos = 0
+cont = 0
 
-''' ******************** Alarmas ******************** '''
+''' ******************** Alarmas y botones ******************** '''
 
-alarm = Div(text='<div class="container"><h2>¡ALARMA!</h2></div>')
+alarm = Div(text='<div class="container"><h2>¡ALARMA!</h2><img src="InterfazGrafica/static/alarm.png"></div>')
 alarm.visible = False
 
+dataRecordingButton = Button(label="Comenzar a adquirir datos", button_type="success")
+dataRecordingLabel = Div(text='<p>Adquiriendo datos...</p>')
+dataRecordingLabel.visible = False
+
+extensionsMenu = [("csv", "csv"), ("txt", "txt"), ("npy", "npy")]
+extensionsDropdown = Dropdown(label="Guardar los datos con extensión:", button_type="success", menu=extensionsMenu)
+
 ''' ******************** Modo Automático ******************** '''
-label1 = Div(text='<h1>Modo Automático</h1>')
-refEst1 = Slider(title="Altura de Referencia Estanque 1", value=ref1, start=0.0,
+
+label1 = Div(text='<h1>Modo Automático &#9889;</h1><hr>')
+refEst1 = Slider(title="Altura de Referencia Estanque 1", value=40, start=0.0,
                  end=50.0, step=0.1)
-refEst2 = Slider(title="Altura de Referencia Estanque 2", value=ref2, start=0.0,
+refEst2 = Slider(title="Altura de Referencia Estanque 2", value=40, start=0.0,
                  end=50.0, step=0.1)
 
-Kp1 = TextInput(title="Constante Proporcional V1", value='0')
-Ki1 = TextInput(title="Constante Integral V1", value='0')
-Kd1 = TextInput(title="Constante Derivativa V1", value='0')
+valvula1Label = Div(text='<h3>Válvula 1</h3><hr>')
+Kp1 = TextInput(title="Constante Proporcional", value='0')
+Ki1 = TextInput(title="Constante Integral", value='0')
+Kd1 = TextInput(title="Constante Derivativa", value='0')
 
-Kp2 = TextInput(title="Constante Proporcional V2", value='0')
-Ki2 = TextInput(title="Constante Integral V2", value='0')
-Kd2 = TextInput(title="Constante Derivativa V2", value='0')
+valvula2Label = Div(text='<h3>Válvula 2</h3><hr>')
+Kp2 = TextInput(title="Constante Proporcional", value='0')
+Ki2 = TextInput(title="Constante Integral", value='0')
+Kd2 = TextInput(title="Constante Derivativa", value='0')
 
 ''' ******************** Modo Manual ******************** '''
-label2 = Div(text='<h1>Modo Manual</h1>')
-voltageV1 = Slider(title="Voltaje Válvula 1", value=vol1, start=-1, end=1,
+
+label2 = Div(text='<h1>Modo Manual &#9997;</h1><hr>')
+voltageV1 = Slider(title="Voltaje Válvula 1", value=vol1, start=-5.0, end=5.0,
                    step=0.01)
-voltageV2 = Slider(title="Voltaje Válvula 2", value=vol2, start=-1, end=1,
+voltageV2 = Slider(title="Voltaje Válvula 2", value=vol2, start=-5.0, end=5.0,
                    step=0.01)
-razonFlujoV1 = Slider(title="Razón de Flujo Válvula 1", value=gamma1, start=0.01,
+razonFlujoV1 = Slider(title="Razón de Flujo Válvula 1", value=0.0, start=0.0,
                       end=0.99, step=0.01)
-razonFlujoV2 = Slider(title="Razón de Flujo Válvula 2", value=gamma2, start=0.0,
+razonFlujoV2 = Slider(title="Razón de Flujo Válvula 2", value=0.0, start=0.0,
                       end=0.99, step=0.01)
 
 ''' ******************** Figures ******************** '''
@@ -136,10 +159,12 @@ fig_vol2.xaxis.axis_label = 'Tiempo (S)'
 fig_vol2.yaxis.axis_label = '[V]'
 fig_vol2.legend.location = "top_left"
 
+''' ******************** Main Loop ******************** '''
+
 
 # Funcion principal que se llama cada cierto tiempo para mostrar la informacion
 def MainLoop():  # Funcion principal que se llama cada cierto tiempo para mostrar la informacion
-    global t, ref1, ref2, automatico
+    global t, ref1, ref2, automatico, cont
 
     h1 = cliente.alturas['H1'].get_value()
     h2 = cliente.alturas['H2'].get_value()
@@ -161,6 +186,12 @@ def MainLoop():  # Funcion principal que se llama cada cierto tiempo para mostra
 
     DataSource_tanques.stream(new_data=update, rollover=200)
 
+    if datos_a_guardar is not None:
+        g1 = cliente.razones['razon1'].get_value()
+        g2 = cliente.razones['razon2'].get_value()
+        datos_a_guardar.loc[cont] = [t, h1, h2, h3, h4, ref11, ref22, 0, 0, 0, 0, 0, 0, 0, 0, v1, v2, g1, g2]
+        cont += 1
+
     t += 1
 
 
@@ -170,10 +201,15 @@ layout = layout([
     [fig_vol1, fig_vol2]
 ])
 
-panel1 = Panel(child=row(Column(label1, refEst1, refEst2, alarm, Kp1, Ki1, Kd1, Kp2, Ki2, Kd2), layout,
-                         sizing_mode='fixed'), title='Modo Automático')
-panel2 = Panel(child=row(Column(label2, voltageV1, voltageV2, razonFlujoV1,
-                                razonFlujoV2, alarm), layout, sizing_mode='fixed'), title='Modo Manual')
+panel1 = Panel(child=row(
+    Column(label1, row(Column(dataRecordingButton, dataRecordingLabel), Column(extensionsDropdown)), refEst1, refEst2,
+           row(Column(valvula1Label, Kp1,
+                      Ki1, Kd1), Column(valvula2Label, Kp2, Ki2, Kd2)), alarm),
+    layout), title='Modo Automático')
+panel2 = Panel(child=row(
+    Column(label2, row(Column(dataRecordingButton, dataRecordingLabel), Column(extensionsDropdown)),
+           row(Column(valvula1Label, voltageV1, razonFlujoV1),
+               Column(valvula2Label, voltageV2, razonFlujoV2)), alarm), layout), title='Modo Manual')
 
 # Tabs
 tabs = Tabs(tabs=[panel1, panel2])
@@ -182,7 +218,11 @@ tabs = Tabs(tabs=[panel1, panel2])
 
 textInputs = [Kp1, Ki1, Kd1, Kp2, Ki2, Kd2]
 
+sliderInputs = [refEst1, refEst2, voltageV1, voltageV2, razonFlujoV1,
+                razonFlujoV2]
 
+
+# Texto
 def textChanges(attr, old, new):
     '''
     Get excecuted when a text input changes
@@ -197,6 +237,7 @@ def textChanges(attr, old, new):
     print('kd:', kd1)
 
 
+# Sliders
 def slider_changes_ref1(attr, old, new):
     global ref1
     ref1 = new
@@ -243,22 +284,66 @@ def slider_changes_razon2(attr, old, new):
 razonFlujoV2.on_change('value', slider_changes_razon2)
 
 
+# Button
+def recordingButtonClicked():
+    global datos_a_guardar, extension, n_archivos, cont
+    # We were recording the data
+    if dataRecordingLabel.visible:
+        dataRecordingButton.button_type = 'success'
+        dataRecordingButton.label = 'Comenzar a adquirir datos'
+        if extension == 'csv':
+            datos_a_guardar.to_csv('Datos_{}.csv'.format(n_archivos))
+        elif extension == 'txt':
+            tfile = open('Datos_{}.txt'.format(n_archivos), 'a')
+            tfile.write(datos_a_guardar.to_string())
+            tfile.close()
+        elif extension == 'npy':
+            aux = datos_a_guardar.to_numpy()
+            np.save('Datos_{}.npy'.format(n_archivos), aux)
+        datos_a_guardar = None
+        n_archivos += 1
+        cont = 0
+    else:
+        dataRecordingButton.button_type = 'danger'
+        dataRecordingButton.label = 'Dejar de adquirir datos'
+        datos_a_guardar = pan.DataFrame(columns=['Tiempo', 'Tanque1', 'Tanque2', 'Tanque3', 'Tanque4', 'Ref1', 'Ref2',
+                                                 'kp1', 'ki1', 'kd1', 'kw1', 'kp2', 'ki2', 'kd2', 'kw2', 'V1', 'V2',
+                                                 'Gamma1', 'Gamma2'])
+
+    dataRecordingLabel.visible = not dataRecordingLabel.visible
+
+
+dataRecordingButton.on_click(recordingButtonClicked)
+
+
+# Dropdown
+def extensionsDropdownChanged(event):
+    global extension
+    extension = event.item
+
+
+extensionsDropdown.on_event(MenuItemClick, extensionsDropdownChanged)
+
+
+# Tabs
 def panelActive(attr, old, new):
-    global automatico
     '''
     Get excecuted when the other tab is selected
     Changes the operation mode if a tab is changed
     '''
     if tabs.active == 0:
-        automatico = True
+        print('Modo automático activado')
+
     elif tabs.active == 1:
-        automatico = False
+        print('Modo manual activado')
 
 
 tabs.on_change('active', panelActive)
 
 for text in textInputs:
     text.on_change('value', textChanges)
+
+''' ******************** Curdoc ******************** '''
 
 curdoc().add_root(tabs)
 curdoc().title = 'Experiencia 1: Control de Procesos'
